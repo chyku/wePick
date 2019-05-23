8cfe05f9372f31b7eee8b604626ffd599ddfc673
kelley <kelleyc2@illinois.edu>
2019-05-23 13:30:56 -0400

    Adding endpoints

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -3,14 +3,25 @@ import firebase from 'firebase'
 // Get a reference to the database service
 var database = firebase.database();
 
-function writeUserData(userId, name, email, imageUrl) {
-    firebase.database().ref('users/' + userId).set({
-        username: name,
-        email: email,
-        profile_picture : imageUrl
-    });
+// TODO: add receipt info 
+function createNewGroup(adminId, name, bankId) {
+    // Generates a random 6 digit number for the group
+    var groupId = Math.floor(100000 + Math.random() * 900000);
+    firebase.database().ref('groups/' + groupId + 'users/' + adminId).set({
+        bank_id: bankId,
+        name: name,
+        finished: false,
+        is_admin: true
+    })
 }
 
-function createNewGroup() {
-    
+function addUserToGroup(groupId, userId, name, bankId) {
+    firebase.database().ref('groups/' + groupId + 'users/' + userId).set({
+        bank_id: bankId,
+        name: name,
+        finished: false,
+        is_admin: false
+    })
 }
+
+function finishProcess()
