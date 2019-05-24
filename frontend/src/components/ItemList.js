d3817d4db2f578f695b3d30159d7b21bd2d62a9e
kelley <kelleyc2@illinois.edu>
2019-05-24 04:32:59 -0400

    Firebase selection

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
 import firebase from '../utils/firebase-setup';
+import Button from '@material-ui/core/Button';
 import queryString from 'query-string';
 
 class ItemList extends Component {
@@ -33,14 +34,17 @@ class ItemList extends Component {
         var text = firebase.database().ref('groups/' + this.state.groupId + '/receipt');
         text.off('value', this.getText);
     }
-            // snapshot.val().forEach(block => {
-            //     console.log(block);
-            // });
 
     selectItem(itemId) {
         var text = firebase.database().ref('groups/' + this.state.groupId + '/receipt/' + itemId + '/user');
         text.set(this.state.userId);
     }
+
+    unselectItem(itemId) {
+        var text = firebase.database().ref('groups/' + this.state.groupId + '/receipt/' + itemId + '/user');
+        text.remove();
+    }
+
     // need other stuff for when other people select items
     // logic against selecting other ppls items
     // assigning colors to each user - generate rgb or some shit idfk
@@ -49,12 +53,29 @@ class ItemList extends Component {
         
         return (
             <div>
-                <p>
-                    {this.state.userId}
-                </p>
-                <p>
-                    {this.state.groupId}
-                </p>
+                <div>
+                    {this.state.text.map((item, index) => {
+                        return (
+                        item.user ?
+                            (item.user == this.state.userId) ?
+                                (<Button variant="contained" color="secondary" id={index} onClick={() => this.unselectItem(index)}>
+                                    {"Name: " + item.name + " Price:" + item.price}
+                                </Button>)
+                                :
+                                (<Button variant="contained" color="primary" id={index} onClick={() => this.selectItem(index)}>
+                                    {"Name: " + item.name + " Price:" + item.price}
+                                </Button>)
+                            :
+                            (<Button variant="contained" id={index} onClick={() => this.selectItem(index)}>
+                                    {"Name: " + item.name + " Price:" + item.price}
+                            </Button>)
+                    )})}
+                </div>
+                <div>
+                    <Button variant="contained" color="secondary">
+                        Enter
+                    </Button>
+                </div>
             </div>
         );
     }
