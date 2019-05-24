737fb69cacfc7c9278c9b7deb9b33a31c4b4ba7a
kelley <kelleyc2@illinois.edu>
2019-05-24 11:59:39 -0400

    Got rid of redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';import {combineReducers} from 'redux';
-import data from './redux/reducers/data';
-import index from './redux/reducers/index';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import reduxThunk from 'redux-thunk';
-import reducers from './redux/reducers';
+import App from './components/App';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById("root")
 );
